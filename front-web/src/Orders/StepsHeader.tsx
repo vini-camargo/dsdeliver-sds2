748e45cfe5db6cf0b15f54e254c496508b05d9ae
Vinicius Camargo <vicamargofranca@gmail.com>
2021-01-10 14:46:16 -0300

    Fix on StepsHeader text: 'Enviar' -> 'Fazer'

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,24 +1,24 @@
 function StepsHeader() {
-    return (
-        <header className="orders-steps-container">
-            <div className="orders-steps-content">
-                <h1 className="steps-title">
-                    SIGA AS <br />
+  return (
+    <header className="orders-steps-container">
+      <div className="orders-steps-content">
+        <h1 className="steps-title">
+          SIGA AS <br />
                     ETAPAS
                 </h1>
-                <ul className="steps-items">
-                    <li>
-                        <span className="steps-number">1</span>
+        <ul className="steps-items">
+          <li>
+            <span className="steps-number">1</span>
                         Selecione os produtos e localização
                     </li>
-                    <li>
-                        <span className="steps-number">2</span>
-                        Depois clique em <strong>"ENVIAR PEDIDO"</strong>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    )
+          <li>
+            <span className="steps-number">2</span>
+                        Depois clique em <strong>"FAZER PEDIDO"</strong>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
 }
 
 export default StepsHeader;
